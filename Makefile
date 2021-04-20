e68f3190965e9fe72874f187eeb252af9e0013a6
Luca Zefran <lzefran2@illinois.edugit config --global user.email lzefran2@illinois.edu>
2021-04-20 12:32:23 -0500

    Added Makefile and main file

---
+++ Makefile
@@ -0,0 +1,9 @@
+EXE = project
+TEST=test
+
+EXE_OBJ=project.o
+OBJS=project.o
+
+OBJDIR_COPY = .objs/
+
+include cs225/make/cs225.mk
---
+++ project.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    std::cout<<"TESTING MAKE FILE"<<std::endl;
+    return 1;
+}
