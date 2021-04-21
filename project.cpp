cac9177977e080105e7c6a8b898fc66648298015
Luca Zefran <lzefran2@illinois.edugit config --global user.email lzefran2@illinois.edu>
2021-04-21 18:56:41 -0500

    One Graph

--- project.cpp
+++ project.cpp
@@ -11,33 +11,28 @@ int main()
   std::vector<std::vector<std::string>> ECE_data=getData("Courses-and-prereqs/ECE_courses.dat");
   std::vector<std::vector<std::string>> MATH_data=getData("Courses-and-prereqs/MATH_courses.dat");
   std::vector<std::vector<std::string>> PHYS_data=getData("Courses-and-prereqs/PHYS_courses.dat");
-  std::vector<Vertex> ECE_Vertices;
-  std::vector<Vertex> CS_Vertices;
-  std::vector<Vertex> PHYS_Vertices;
-  std::vector<Vertex> MATH_Vertices;
+  std::vector<Vertex> Vertices;
   for(unsigned i=0;i<ECE_data.size();i++)
   {
     std::vector<std::string> firstTok=ECE_data[i];
-    ECE_Vertices.push_back(Vertex(firstTok[0]));
+    Vertices.push_back(Vertex(firstTok[0]));
   }
   for(unsigned i=0;i<CS_data.size();i++)
   {
     std::vector<std::string> firstTok=CS_data[i];
-    CS_Vertices.push_back(Vertex(firstTok[0]));
+    Vertices.push_back(Vertex(firstTok[0]));
   }
   for(unsigned i=0;i<MATH_data.size();i++)
   {
     std::vector<std::string> firstTok=MATH_data[i];
-    MATH_Vertices.push_back(Vertex(firstTok[0]));
+    Vertices.push_back(Vertex(firstTok[0]));
   }
   for(unsigned i=0;i<PHYS_data.size();i++)
   {
     std::vector<std::string> firstTok=PHYS_data[i];
-    PHYS_Vertices.push_back(Vertex(firstTok[0]));
+    Vertices.push_back(Vertex(firstTok[0]));
   }
-  Graph ECE_Graph(ECE_Vertices);
-  Graph CS_Graph(CS_Vertices);
-  Graph PHYS_Graph(PHYS_Vertices);
-  Graph MATH_Graph(MATH_Vertices);
+  Graph g(Vertices);
+  std::cout<<Vertices.size()<<std::endl;
   return 0;
 }
