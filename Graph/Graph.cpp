88a4e0b13be0e94a933d01b44f869a911aa221ce
David Messerschmidt <davidm12@illinois.edu>
2021-04-21 18:26:38 -0500

    merge lol

--- Graph/Graph.cpp
+++ Graph/Graph.cpp
@@ -1,13 +1,9 @@
 #include "Graph.h"
 #include <iostream>
-Graph::Graph(){
-<<<<<<< HEAD
-    std::cout << "Hi" << std::endl;
+Graph::Graph()
+{
+    start_ = new Vertex("Start");
 }
-
-
-
-
 
 std::vector<Vertex> & Graph::getVertices()
 {
@@ -26,8 +22,3 @@ size_t Graph::getEdgesSize()
 {
     retrurn edges_.size();
 }
-=======
-    start_ = new Vertex("Start");
-}
-
->>>>>>> 0d05876366565760e60bd28580161d1be4a7aed1
