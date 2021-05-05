96fdb5c3220d8c078d9784b6878c21c2e753416b
David Messerschmidt <davidm12@illinois.edu>
2021-05-05 12:05:34 -0500

    first working BFS, will make comprehensive tests soon

--- project.cpp
+++ project.cpp
@@ -8,34 +8,42 @@ using namespace std;
 int main()
 {
   ClassGraph g;
+
+
   std::cout << std::endl;
   std::cout << std::endl;
   std::cout << "#####################################   Welcome to the danger zone  --  (Start of main)   ######################################" << std::endl;
   std::cout<< "# of verts in graph: " << g.getVertices().size()<<std::endl;
 
+  std::cout << std::endl;
+  std::cout << std::endl;
+  std::cout << "------------------------------------------------------" << std::endl;
+
   std::vector<Vertex*> & verts = g.getVertices();  //get reference to vertices
-  Vertex* start = g.getStart();
-  std::cout << "Printing First 10 vertexes in graph obj: " << std::endl;
+  //Vertex* start = g.getStart();
+  /*
+    std::cout << "Printing First 15 vertexes in graph obj: " << std::endl;
+    int count = 0;
+    for (std::vector<Vertex*>::iterator it = verts.begin(); it != verts.end(); ++it)
+    {
+      std::cout << count << " " << **it << std::endl;
+      ++count;
+      if (count == 15)
+        break;
 
-  int count = 0;
-  for (std::vector<Vertex*>::iterator it = verts.begin(); it != verts.end(); ++it)
-  {
-    std::cout << *it << std::endl;
-    ++count;
-    if (count == 10)
-      break;
+    }
+  */
 
-  }
-
+  //Vertex* start = g.getStart();
+  Vertex* start = verts[0];
 
   BFS traversal(&g, start);                    //create traversal, start at vert 0
-
-  for (BFS::Iterator it = traversal.begin(); it != traversal.end(); ++it)
+  std::cout << "Classes that require " << start->getName() << ":" << std::endl;
+  BFS::Iterator it = traversal.begin();
+  ++it;
+  for (; it != traversal.end(); ++it)
   {
-    std::cout << "test" << std::endl;
-    std::cout << **it << std::endl;     //dereferences the iterator (which is *Vertex), 
-                                        //then dereference that and use its std::out functionality
-    std::cout << "test" << std::endl;   
+    std::cout << **it << std::endl; 
   }
   return 0;
 
