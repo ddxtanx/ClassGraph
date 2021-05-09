bea6390d2ce93c62d0813676e73a12a662fd9e18
Luca Zefran <lzefran2@illinois.edugit config --global user.email lzefran2@illinois.edu>
2021-05-09 13:55:19 -0500

    Spanning Tree Makefile

--- Makefile
+++ Makefile
@@ -3,12 +3,12 @@ EXE = project
 TEST=test
 
 EXE_OBJ=project.o
-OBJS=project.o utils.o ClassGraph.o dataConvert.o  SpanningTree.o
-
+OBJS=project.o utils.o ClassGraph.o dataConvert.o
 include Graph/graph.mk
 include Davids_Work/david.mk
 include cs225/make/cs225.mk
 include LGD/LGD.mk
+include SpanningTree/SpanningTree.mk
 
 clean:
 	find .objs -type f -name "*.[d|o]" -exec rm {} \;
---
+++ SpanningTree/SpanningTree.h
@@ -0,0 +1,20 @@
+#pragma once
+#include "Graph/Graph.h"
+#include "Graph/Edge.h"
+#include "Graph/Vertex.h"
+#include "ClassGraph.h"
+#include <string>
+#include <vector>
+#include "Davids_Work/BFS.h"
+
+class SpanningTree : public Graph{
+    public:
+        SpanningTree(ClassGraph& other);
+        Vertex*& getStart();
+        Vertex*& getEnd();
+        void makeAcyclic();
+    private:
+        Vertex* TreeStart;
+        std::vector<Edge> Edges;
+        std::vector<Vertex*> vertices;
+};
